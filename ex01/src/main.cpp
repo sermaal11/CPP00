b8aa6c3e112d3cb1eed05f89f9bde0cd1a991ed3
Jariskan <sergio.marin.alvarez@gmail.com>
2025-07-09 12:25:19 +0200

    Update main and utils files with improved input handling and validation

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sergio <sergio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 10:33:06 by sergio            #+#    #+#             */
-/*   Updated: 2025/07/09 12:18:51 by sergio           ###   ########.fr       */
+/*   Updated: 2025/07/09 12:23:26 by sergio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,17 @@ int main (int argc, char **argv)
     if (argc == 1)
     {
         initPhoneBook();
-
         while (true)
         {
             std::cout << GREEN << "WAITING COMMAND:\n" << RESET;
             std::cin >> input;
-
-            // Controlar EOF (Ctrl+D o Ctrl+Z)
             if (!std::cin) {
                 std::cout << RED << "Input stream closed. Exiting...\n" << RESET;
                 break;
             }
-
-            // Limpiar el resto del buffer
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-            // Convertir a mayúsculas (C++98 compatible)
             for (size_t i = 0; i < input.length(); ++i)
                 input[i] = std::toupper(input[i]);
-
             if (input == "ADD")
                 phoneBook.add();
             else if (input == "SEARCH")
@@ -52,9 +44,6 @@ int main (int argc, char **argv)
         }
     }
     else
-    {
         std::cout << RED << "This program does not accept arguments.\n" << RESET;
-    }
-
     return 0;
 }
--- ex01/src/utils.cpp
+++ ex01/src/utils.cpp
@@ -6,14 +6,13 @@
 /*   By: sergio <sergio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 11:32:33 by sergio            #+#    #+#             */
-/*   Updated: 2025/07/09 12:19:49 by sergio           ###   ########.fr       */
+/*   Updated: 2025/07/09 12:25:01 by sergio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/contact.hpp"
 #include "../include/phonebook.hpp"
 
-// Muestra el mensaje inicial del programa
 void initPhoneBook()
 {
     std::cout << CYAN << "PHONE BOOK\n" << RESET;
@@ -25,8 +24,6 @@ void initPhoneBook()
             << RESET;
 }
 
-// Solicita al usuario un campo y valida que no esté vacío
-// También valida que el número de teléfono tenga 9 dígitos exactos si aplica
 std::string completeContact(std::string fieldName)
 {
     std::string input;
@@ -34,14 +31,12 @@ std::string completeContact(std::string fieldName)
     {
         std::cout << fieldName;
         std::getline(std::cin, input);
-
+        
         if (!std::cin)
         {
             std::cout << RED << "\nInput interrupted. Exiting...\n" << RESET;
             exit(1);
         }
-
-        // Verificar que no sea todo espacios
         bool hasNonSpace = false;
         for (std::size_t i = 0; i < input.length(); ++i)
         {
@@ -56,13 +51,9 @@ std::string completeContact(std::string fieldName)
             std::cout << RED << "The field cannot be empty. Please try again.\n" << RESET;
             continue;
         }
-
-        // Eliminar espacios al inicio y al final
         std::size_t start = input.find_first_not_of(" \t");
         std::size_t end = input.find_last_not_of(" \t");
         input = input.substr(start, end - start + 1);
-
-        // Validar número de teléfono si aplica
         if (fieldName == "Phone Number: ")
         {
             bool onlyDigits = true;
@@ -74,25 +65,21 @@ std::string completeContact(std::string fieldName)
                     break;
                 }
             }
-
             if (!onlyDigits)
             {
                 std::cout << RED << "Phone number must contain only digits.\n" << RESET;
                 continue;
             }
-
             if (input.length() != 9)
             {
                 std::cout << RED << "Phone number must contain exactly 9 digits.\n" << RESET;
                 continue;
             }
         }
-
         return input;
     }
 }
 
-// Formatea un campo a exactamente 10 caracteres: truncando o alineando a la derecha
 std::string formatField(const std::string &field)
 {
     if (field.length() > 10)
