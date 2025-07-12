9d5a819ea509537d148e2a1fc702103a7c5983c2
Jariskan <sergio.marin.alvarez@gmail.com>
2025-07-12 14:10:24 +0200

    Update .gitignore and utils.hpp; replace MyAwesome entries with BraiiiiiiinnnzzzZ and adjust update timestamp

--- ex01/include/utils.hpp
+++ ex01/include/utils.hpp
@@ -6,7 +6,7 @@
 /*   By: sergio <sergio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 12:11:16 by sergio            #+#    #+#             */
-/*   Updated: 2025/07/09 12:21:37 by sergio           ###   ########.fr       */
+/*   Updated: 2025/07/11 19:34:26 by sergio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@
 # include <string>
 
 # define RED "\033[31m"
-# define RESET "\033[0m"
 # define CYAN "\033[36m"
 # define MAGENTA "\033[35m"
 # define GREEN "\033[32m"
+# define RESET "\033[0m"
 
 std::string completeContact(std::string fieldName);
 void initPhoneBook();
